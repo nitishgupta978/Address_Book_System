c8f614c5987c6f590a43759fd2057934c480aa68
JohnWick00 <98822014+nitishgupta978@users.noreply.github.com>
2022-04-30 10:29:36 +0530

    update

--- src/Com/bridgelabz/test/AppTest.java
+++ src/Com/bridgelabz/test/AppTest.java
@@ -1,5 +1,22 @@
 package Com.bridgelabz.test;
+import static org.junit.jupiter.api.Assertions.*;
 
-public class AppTest {
+	import org.junit.jupiter.api.Test;
+
+	@SuppressWarnings("unused")
+	public class AppTest {
+		
+		
+		    @Test
+		    public void shouldAnswerWithTrue()
+		    {
+		        assertTrue( true );
+		    }
+
+			private void assertTrue(boolean b) {
+				// TODO Auto-generated method stub
+				
+			}
+}
 
 }
